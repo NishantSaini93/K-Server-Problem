c034ff21764b127da6cc90dc6a2c7ecb7d2a5014
NishantSaini93 <nishantsainins@outlook.com>
2019-06-07 10:29:31 -0400

    Contains.hpp updated

--- Contains.hpp
+++ Contains.hpp
@@ -2,5 +2,9 @@
 #define _Contains_CPP_INCLUDED
 #include<iostream>
 using namespace std;
+/**
+*This function checks if current position of servers covers the current request
+*@return int 1 if contains otherwise 0
+*/
 int performContains(int currentRequest,int currentConfiguration[]);
 #endif
